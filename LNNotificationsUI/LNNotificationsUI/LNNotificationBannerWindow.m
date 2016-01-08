299a77859bd6d81d0e2551eff98d4f005cae4701
FelixII <felix.lisczyk@gmail.com>
2016-01-08 15:45:03 +0100

    Update LNNotificationBannerWindow.m

--- LNNotificationsUI/LNNotificationsUI/LNNotificationBannerWindow.m
+++ LNNotificationsUI/LNNotificationsUI/LNNotificationBannerWindow.m
@@ -12,7 +12,7 @@
 #import "LNNotificationCenter.h"
 
 static const NSTimeInterval LNNotificationAnimationDuration = 0.5;
-static const NSTimeInterval LNNotificationFullDuration = 5.0;
+static const NSTimeInterval LNNotificationFullDuration = 10.0;
 static const NSTimeInterval LNNotificationCutOffDuration = 2.5;
 
 static const CGFloat LNNotificationViewHeight = 68.0;
